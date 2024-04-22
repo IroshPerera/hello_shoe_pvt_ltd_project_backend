ae7c3a0aee6fee9a2b561b182329eca641ea047a
Irosh Perera <iroshlashan31@gmail.com>
2024-04-23 00:39:34 +0530

    add employee api

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Employee.java
@@ -0,0 +1,47 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.EmployeeDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+@RequiredArgsConstructor
+public class Employee {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Hello I'm Employee Controller. I'm OK! Have a nice day!";
+    }
+
+    @PostMapping
+    public boolean saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        return employeeService.add(employeeDTO);
+    }
+
+    @PutMapping
+    public boolean updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        return employeeService.update(employeeDTO);
+    }
+
+    @DeleteMapping
+    public boolean deleteEmployee(@RequestParam String employee_code) {
+        return employeeService.delete(employee_code);
+    }
+
+    @GetMapping
+    public EmployeeDTO searchEmployee(@RequestParam String employee_code) {
+        return employeeService.search(employee_code);
+    }
+
+    @GetMapping("/all")
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeService.getAll();
+    }
+}
