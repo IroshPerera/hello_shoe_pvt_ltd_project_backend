29c838ad68d5d131d193ea1db9eaf188b9d63810
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:51:45 +0530

    add test cases

---
+++ src/test/java/lk/ijse/gdse/hello_shoe_pvt_ltd/HelloShoePvtLtdApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoePvtLtdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
