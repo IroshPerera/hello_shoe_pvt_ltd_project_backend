dc6f365f2f16be2d956c20a63dc60c795afc5414
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:45:41 +0530

    add customer api

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Customer.java
@@ -0,0 +1,48 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+public class Customer {
+
+    private final CustomerService customerService;
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Hello I'm Customer Controller. I'm OK! Have a nice day!";
+    }
+
+    @PostMapping
+    public boolean saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        return customerService.add(customerDTO);
+    }
+
+    @PutMapping
+    public boolean updateCustomer(@RequestBody CustomerDTO customerDTO) {
+       return customerService.update(customerDTO);
+    }
+
+    @DeleteMapping
+    public boolean deleteCustomer(@RequestParam String customer_code) {
+        return customerService.delete(customer_code);
+    }
+
+    @GetMapping
+    public CustomerDTO searchCustomer(@RequestParam String customer_code) {
+        return customerService.search(customer_code);
+    }
+
+    @GetMapping("/all")
+    public List<CustomerDTO> getAllCustomers() {
+        return customerService.getAll();
+    }
+
+}
