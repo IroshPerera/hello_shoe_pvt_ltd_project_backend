2803fc3a77e11febbaec04eb407544b7186e22f9
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:46:20 +0530

    add supplier repo

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dao/SupplierRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.dao;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.SupplierEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SupplierRepo extends JpaRepository<SupplierEntity,String> {
+}
