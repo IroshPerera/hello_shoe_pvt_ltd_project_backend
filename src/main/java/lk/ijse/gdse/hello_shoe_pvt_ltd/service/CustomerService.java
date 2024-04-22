184acac0712642f0f92d90fc8683880c7a3fa9ff
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:47:38 +0530

    add customer service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/CustomerService.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
+
+public interface CustomerService extends SuperService<String, CustomerDTO>{
+}
