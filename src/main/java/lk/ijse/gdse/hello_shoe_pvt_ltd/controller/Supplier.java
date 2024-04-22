d479f92e5f5f88b81e8b21266453c8ee3d0aacf9
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:45:49 +0530

    add supplier api

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Supplier.java
@@ -0,0 +1,47 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SupplierDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.SupplierService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/supplier")
+@RequiredArgsConstructor
+public class Supplier {
+
+    private final SupplierService supplierService;
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Hello I'm Supplier Controller. I'm OK! Have a nice day!";
+    }
+
+    @PostMapping
+    public boolean saveSupplier(@RequestBody SupplierDTO supplierDTO) {
+        return supplierService.add(supplierDTO);
+    }
+
+    @PutMapping
+    public boolean updateSupplier(@RequestBody SupplierDTO supplierDTO) {
+        return supplierService.update(supplierDTO);
+    }
+
+    @DeleteMapping
+    public boolean deleteSupplier(@RequestParam String supplier_code) {
+        return supplierService.delete(supplier_code);
+    }
+
+    @GetMapping
+    public SupplierDTO searchSupplier(@RequestParam String supplier_code) {
+        return supplierService.search(supplier_code);
+    }
+
+    @GetMapping("/all")
+    public List<SupplierDTO> getAllSuppliers() {
+        return supplierService.getAll();
+    }
+
+}
