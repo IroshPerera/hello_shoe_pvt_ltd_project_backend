20bf8ea841d791feaf758003f617c691c12416df
Irosh Perera <iroshlashan31@gmail.com>
2024-04-23 00:39:57 +0530

    add employee service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/EmployeeService.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.EmployeeDTO;
+
+public interface EmployeeService extends SuperService<String, EmployeeDTO> {
+}
