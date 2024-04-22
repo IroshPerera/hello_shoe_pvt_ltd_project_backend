8a88f6dec57ceeb065c86f006edacf831fbdc09f
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:46:58 +0530

    add supplier dto

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/SupplierDTO.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.dto;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SupplierDTO implements SuperDTO{
+    private String supplier_code;
+    private String name;
+    private Category category;
+    private String building_number;
+    private String lane;
+    private String city;
+    private String state;
+    private String postal_code;
+    private String country;
+    private String mobile_contact;
+    private String landline_contact;
+    private String email;
+
+}
