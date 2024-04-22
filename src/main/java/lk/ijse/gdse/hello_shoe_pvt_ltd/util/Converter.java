a3760e184b0a38e7b0a1d603a965dea8f5fbb307
Irosh Perera <iroshlashan31@gmail.com>
2024-04-23 00:28:39 +0530

    add employee converter

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/util/Converter.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/util/Converter.java
@@ -1,13 +1,17 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.util;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.EmployeeDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SupplierDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.CustomerEntity;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.EmployeeEntity;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.SupplierEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Converter {
+
+    //set customer dtos values to temCustomer entity
     public void convertCustomerEntity(CustomerDTO customerDTO, CustomerEntity tmpCustomerEntity) {
         tmpCustomerEntity.setCustomer_code(customerDTO.getCustomer_code());
         tmpCustomerEntity.setName(customerDTO.getName());
@@ -27,6 +31,7 @@ public class Converter {
 
     }
 
+    //set supplier dtos values to temSupplier entity
     public void convertSupplierEntity(SupplierDTO supplierDTO, SupplierEntity tmpSupplierEntity) {
         tmpSupplierEntity.setSupplier_code(supplierDTO.getSupplier_code());
         tmpSupplierEntity.setName(supplierDTO.getName());
@@ -40,6 +45,30 @@ public class Converter {
         tmpSupplierEntity.setMobile_contact(supplierDTO.getMobile_contact());
         tmpSupplierEntity.setLandline_contact(supplierDTO.getLandline_contact());
         tmpSupplierEntity.setEmail(supplierDTO.getEmail());
+
+    }
+
+    //set employee entity values to temEmployee dto
+    public void convertEmployeeEntity(EmployeeDTO employeeDTO, EmployeeEntity tmpEmployeeEntity) {
+        tmpEmployeeEntity.setEmployee_code(employeeDTO.getEmployee_code());
+        tmpEmployeeEntity.setName(employeeDTO.getName());
+        tmpEmployeeEntity.setProfile_pic(employeeDTO.getProfile_pic());
+        tmpEmployeeEntity.setGender(employeeDTO.getGender());
+        tmpEmployeeEntity.setStatus(employeeDTO.getStatus());
+        tmpEmployeeEntity.setDesignation(employeeDTO.getDesignation());
+        tmpEmployeeEntity.setRole(employeeDTO.getRole());
+        tmpEmployeeEntity.setDob(employeeDTO.getDob());
+        tmpEmployeeEntity.setJoined_date(employeeDTO.getJoined_date());
+        tmpEmployeeEntity.setBranch(employeeDTO.getBranch());
+        tmpEmployeeEntity.setBuilding_number(employeeDTO.getBuilding_number());
+        tmpEmployeeEntity.setLane(employeeDTO.getLane());
+        tmpEmployeeEntity.setCity(employeeDTO.getCity());
+        tmpEmployeeEntity.setState(employeeDTO.getState());
+        tmpEmployeeEntity.setPostal_code(employeeDTO.getPostal_code());
+        tmpEmployeeEntity.setContact(employeeDTO.getContact());
+        tmpEmployeeEntity.setEmail(employeeDTO.getEmail());
+        tmpEmployeeEntity.setGuardian_name(employeeDTO.getGuardian_name());
+        tmpEmployeeEntity.setGuardian_contact(employeeDTO.getGuardian_contact());
 
     }
 }
