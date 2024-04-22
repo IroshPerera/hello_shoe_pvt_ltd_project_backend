6d2cb452854cf30036470fa47def2892b77454ef
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:47:06 +0530

    add customer dto

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/CustomerDTO.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.dto;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Gender;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Level;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerDTO implements SuperDTO{
+    private String customer_code;
+    private String name;
+    private Gender gender;
+    private Date date;
+    private Level level;
+    private Integer point;
+    private Date dob;
+    private String building_number;
+    private String lane;
+    private String city;
+    private String state;
+    private String postal_code;
+    private String contact;
+    private String email;
+    private Timestamp recent_purchase;
+}
