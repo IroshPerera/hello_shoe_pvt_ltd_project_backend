e2f2e2b9a591dee3e47347a6dad39bc98b733f28
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:47:12 +0530

    add customer entity

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/CustomerEntity.java
@@ -0,0 +1,39 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Gender;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Level;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+
+@Entity
+@Table(name = "customer")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerEntity {
+    @Id
+    private String customer_code;
+    private String name;
+    private Gender gender;
+    private Date date;
+    private Level level;
+    private Integer point;
+    private Date dob;
+    private String building_number;
+    private String lane;
+    private String city;
+    private String state;
+    private String postal_code;
+    private String contact;
+    private String email;
+    private Timestamp recent_purchase;
+
+}
