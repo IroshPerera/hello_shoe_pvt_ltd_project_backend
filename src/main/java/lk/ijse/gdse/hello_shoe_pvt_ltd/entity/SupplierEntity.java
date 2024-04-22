53462e1975bbae66c2527a70027fe41976dbedae
Irosh Perera <iroshlashan31@gmail.com>
2024-04-22 23:47:18 +0530

    add supplier entity

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SupplierEntity.java
@@ -0,0 +1,30 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "supplier")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SupplierEntity {
+    @Id
+    private String supplier_code;
+    private String name;
+    private Category category;
+    private String building_number;
+    private String lane;
+    private String city;
+    private String state;
+    private String postal_code;
+    private String country;
+    private String mobile_contact;
+    private String landline_contact;
+    private String email;
+}
