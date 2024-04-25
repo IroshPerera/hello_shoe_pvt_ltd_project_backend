7f562381981cf84050bcbc439578b160be7da39a
Irosh Perera <iroshlashan31@gmail.com>
2024-04-25 10:19:43 +0530

    few changes

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/CustomerRepo.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/CustomerRepo.java
@@ -2,6 +2,8 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.repository;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepo extends JpaRepository<CustomerEntity, String>{
 }
--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/EmployeeRepo.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/EmployeeRepo.java
@@ -2,6 +2,8 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.repository;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepo extends JpaRepository<EmployeeEntity, String>{
 }
