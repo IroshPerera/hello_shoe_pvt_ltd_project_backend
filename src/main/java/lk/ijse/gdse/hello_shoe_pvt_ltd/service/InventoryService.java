d65b77b19a2ec264a22b35f4e7f508c323abc78e
Irosh Perera <iroshlashan31@gmail.com>
2024-04-25 10:21:04 +0530

    add inventory service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/InventoryService.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
+
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.InventoryDTO;
+
+
+public interface InventoryService extends SuperService<String, InventoryDTO>{
+}
