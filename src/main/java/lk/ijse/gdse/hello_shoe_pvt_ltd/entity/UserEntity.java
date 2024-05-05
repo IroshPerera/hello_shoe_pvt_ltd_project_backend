316387c7c20624f74e7d05a9abaaa8d813044d6f
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:09:41 +0530

    implement user entity

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/UserEntity.java
@@ -0,0 +1,61 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
+
+import jakarta.persistence.*;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class UserEntity implements UserDetails {
+    @Id
+    private String id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority>  authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role.name()));
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
