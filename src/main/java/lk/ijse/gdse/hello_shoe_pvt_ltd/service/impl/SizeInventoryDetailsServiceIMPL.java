38d188b4e236b22f3292f244064626cdc219b741
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:15:59 +0530

    implement size inventory service impl

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/SizeInventoryDetailsServiceIMPL.java
@@ -0,0 +1,50 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SizeInventoryDetailsDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.SizeInventoryDetailsEntity;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.repository.SizeInventoryDetailsRepo;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.SizeInventoryDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SizeInventoryDetailsServiceIMPL implements SizeInventoryDetailsService {
+
+    private final SizeInventoryDetailsRepo sizeInventoryDetailsRepo;
+
+    @Override
+    public boolean add(SizeInventoryDetailsDTO sizeInventoryDetailsDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public boolean update(SizeInventoryDetailsDTO sizeInventoryDetailsDTO) {
+        return false;
+    }
+
+    @Override
+    public SizeInventoryDetailsDTO search(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public List<SizeInventoryDetailsDTO> getAll() {
+        return List.of();
+    }
+    @Transactional
+    public void updateQty(String itemCode, String sizeCode, int qty) {
+        int currentQty = sizeInventoryDetailsRepo.findQtyByItemCodeAndSize(itemCode, sizeCode);
+        int newQty = currentQty + qty;
+        sizeInventoryDetailsRepo.updateQty(itemCode, sizeCode, newQty);
+    }
+}
