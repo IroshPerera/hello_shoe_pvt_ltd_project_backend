325b56d5c0af25e23f61bc3fb2560c30441d3708
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:06:45 +0530

    implement size controller

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/impl/Size.java
@@ -0,0 +1,52 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.controller.impl;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.controller.SizeController;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SizeDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.SizeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/size")
+@RequiredArgsConstructor
+public class Size implements SizeController<String, SizeDTO> {
+
+    private final SizeService sizeService;
+    @Override
+    @PostMapping
+    public boolean saveSize(@RequestBody SizeDTO sizeDTO) {
+        return sizeService.add(sizeDTO);
+    }
+
+    @Override
+    @PutMapping
+    public boolean updateSize(@RequestBody SizeDTO sizeDTO) {
+        return sizeService.update(sizeDTO);
+    }
+
+    @Override
+    @DeleteMapping
+    public boolean deleteSize(@RequestParam String size_id) {
+        return sizeService.delete(size_id);
+    }
+
+    @Override
+    @GetMapping
+    public SizeDTO searchSize(@RequestParam String size_id) {
+        return sizeService.search(size_id);
+    }
+
+    @Override
+    @GetMapping("/all")
+    public List<SizeDTO> getAllSizes() {
+        return sizeService.getAll();
+    }
+
+    @Override
+    @GetMapping("/health")
+    public String healthCheck() {
+       return "Hello I'm Size Controller. I'm OK! Have a nice day!";
+    }
+}
