92616423ba4d064e7937baf62f244d8a351225cd
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 11:56:45 +0530

    update inventory controller

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/InventoryController.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/InventoryController.java
@@ -1,10 +1,12 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
 
 
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.InventoryDetailsDTO;
+
 import java.util.List;
 
 public interface InventoryController<ID,T> extends SuperController{
-    public boolean saveInventory(T inventoryDTO);
+    public boolean saveInventory(InventoryDetailsDTO inventoryDTO);
     public boolean updateInventory(T inventoryDTO);
     public boolean deleteInventory(ID item_code);
     public T searchInventory(ID item_code);
