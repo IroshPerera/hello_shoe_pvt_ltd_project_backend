698e2e27a4073c8cb35219af4c5b67f4de0d8e7e
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:11:24 +0530

    implement return service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/ReturnService.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.ReturnDTO;
+
+public interface ReturnService extends SuperService<String, ReturnDTO>{
+}
