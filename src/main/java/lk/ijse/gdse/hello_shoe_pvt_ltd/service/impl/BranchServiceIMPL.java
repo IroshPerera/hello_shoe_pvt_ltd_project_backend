c3e9d6105d4a338e8ffff6f05899ab4eba39fa9e
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:16:36 +0530

    implement branch service impl

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/BranchServiceIMPL.java
@@ -0,0 +1,67 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.BranchDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.BranchEntity;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.repository.BranchRepo;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.BranchService;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.convert.Converter;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.map.Mapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BranchServiceIMPL implements BranchService {
+
+    private final BranchRepo branchRepo;
+    private final Mapping mapping;
+    private final Converter converter;
+
+    @Override
+    public boolean add(BranchDTO branchDTO) {
+        BranchEntity branchEntity = mapping.mapToBranchEntity(branchDTO);
+        branchRepo.save(branchEntity);
+        return branchRepo.existsById(branchEntity.getBranch_code());
+    }
+
+    @Override
+    public boolean delete(String branch_code) {
+        if (branchRepo.existsById(branch_code)) {
+            branchRepo.deleteById(branch_code);
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean update(BranchDTO branchDTO) {
+        Optional<BranchEntity> tmpBranch = branchRepo.findById(branchDTO.getBranch_code());
+        if (tmpBranch.isPresent()) {
+            converter.convertBranchEntity(branchDTO, tmpBranch.get());
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public BranchDTO search(String branch_code) {
+
+
+        BranchEntity branchEntity = branchRepo.getReferenceById(branch_code);
+        return mapping.mapToBranchDTO(branchEntity);
+
+    }
+
+    @Override
+    public List<BranchDTO> getAll() {
+        List<BranchEntity> branches = branchRepo.findAll();
+        return mapping.mapToBranchDTOList(branches);
+    }
+}
