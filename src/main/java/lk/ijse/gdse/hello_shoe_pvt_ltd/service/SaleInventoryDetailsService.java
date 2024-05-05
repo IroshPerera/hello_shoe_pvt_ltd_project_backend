97041122afd799153ad90d9d69077ebbfe6fb190
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:11:40 +0530

    implement sale inventory details service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SaleInventoryDetailsService.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SaleInventoryDetailsDTO;
+
+import java.util.List;
+
+public interface SaleInventoryDetailsService{
+    public boolean
+    add(List<SaleInventoryDetailsDTO> saleInventoryDetailsDTO);
+
+    public boolean delete(String s) ;
+
+    public boolean update(SaleInventoryDetailsDTO saleInventoryDetailsDTO) ;
+
+    public SaleInventoryDetailsDTO search(String s) ;
+
+    public List<SaleInventoryDetailsDTO> getAll() ;
+}
