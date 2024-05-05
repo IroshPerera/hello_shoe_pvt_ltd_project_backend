035433ab22f7b30354bc8b01e3ec8f6517ccbd2b
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:09:14 +0530

    implement sale entity

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SaleEntity.java
@@ -0,0 +1,29 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "sale")
+public class SaleEntity {
+    @Id
+    private String order_id;
+    @ManyToOne
+    @JoinColumn(name = "customer_code",referencedColumnName = "customer_code")
+    private CustomerEntity customer;
+    private double total_price;
+    private Timestamp purchase_date;
+    private String payment_method;
+    private double added_points;
+    private String cashier_name;
+
+
+}
