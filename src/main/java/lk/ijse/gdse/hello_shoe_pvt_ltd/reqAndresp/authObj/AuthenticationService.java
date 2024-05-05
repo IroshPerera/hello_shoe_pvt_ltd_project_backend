aedc32006c6e4053f2ffffc9730c90e9e37c3654
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:14:50 +0530

    implement authentication service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/reqAndresp/authObj/AuthenticationService.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.authObj;
+
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.response.JwtAuthResponse;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignIn;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignUp;
+
+public interface AuthenticationService {
+    JwtAuthResponse signIn(SignIn signIn);
+    JwtAuthResponse signUp(SignUp signIn);
+    String refreshToken(String token);
+}
