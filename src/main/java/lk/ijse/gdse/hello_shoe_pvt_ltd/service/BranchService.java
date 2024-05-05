37d9e9c768ea63b3ea28ef41b6cd6ad8098dc7f0
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:11:17 +0530

    implement branch service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/BranchService.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.BranchDTO;
+
+public interface BranchService extends SuperService<String, BranchDTO>{
+}
