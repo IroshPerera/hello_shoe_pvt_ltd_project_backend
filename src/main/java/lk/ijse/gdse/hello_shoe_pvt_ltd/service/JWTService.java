fa3f9e1471903734e2da06de4dc98fdf783af045
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:17:16 +0530

    implement jwt service

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/JWTService.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JWTService {
+    String extractUsername(String token);
+    String generateToken(UserDetails userDetails);
+    boolean validateToken(String token, UserDetails userDetails);
+}
