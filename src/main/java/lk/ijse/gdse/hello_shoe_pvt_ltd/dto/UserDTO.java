011e9131f85228f2282e827c5ac7e6f772f69783
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:08:34 +0530

    implement user dto

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.dto;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserDTO implements SuperDTO {
+    private String id;
+    private String email;
+    private String password;
+    private Role role;
+}
