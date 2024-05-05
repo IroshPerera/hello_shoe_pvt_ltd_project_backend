b4a3d3170903c3bedccaea6bea2b2208b8aa80a0
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:15:07 +0530

    implement Jwt auth response

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/reqAndresp/response/JwtAuthResponse.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class JwtAuthResponse {
+    private String token;
+}
