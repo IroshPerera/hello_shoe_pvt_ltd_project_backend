056ab62a3088c4ed156105033f7386be2eef39d8
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 11:56:57 +0530

    update inventory dto

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/InventoryDTO.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/InventoryDTO.java
@@ -1,5 +1,9 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.dto;
 
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Gender;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Occasion;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Verities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,14 +15,8 @@ public class InventoryDTO implements SuperDTO{
     private String item_code;
     private String item_desc;
     private String item_pic;
-    private String category;
-    private Integer size;
-    private String supplier_code;
-    private String supplier_name;
-    private Double buying_price;
-    private Double selling_price;
-    private Double expected_profit;
-    private Double profit_margin;
-    private String status;
-    private Integer qty_on_hand;
+    private Occasion occasion;
+    private Gender gender;
+    private Verities verities;
+    private SupplierDTO supplier;
 }
