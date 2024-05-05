81fbbb0b958e1cebae6e1bfc9288a4dacb582cd5
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:08:05 +0530

    implement sale dto

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/SaleDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class SaleDTO implements SuperDTO{
+    private String order_id;
+   /* private String item_code;*/
+   /* private String item_desc;*/
+   /* private int size;*/
+   /* private double unit_price;*/
+  /*  private int item_qty;*/
+    private double total_price;
+    private Timestamp purchase_date;
+    private String payment_method;
+    private double added_points;
+    private String cashier_name;
+    private CustomerDTO customer;
+}
