6f1b2f25a065a0f2c7f8d97c4c32796278f65c1f
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 11:57:19 +0530

    update inventory service

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/InventoryService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/InventoryService.java
@@ -2,7 +2,9 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.InventoryDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.InventoryDetailsDTO;
 
 
 public interface InventoryService extends SuperService<String, InventoryDTO>{
+    boolean add(InventoryDetailsDTO inventoryDetailsDTO) ;
 }
