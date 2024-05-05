e31c9198fe2985158a3b20f4a7b36f701d757217
Irosh Perera <iroshlashan31@gmail.com>
2024-05-05 15:06:52 +0530

    implement user controller

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/impl/User.java
@@ -0,0 +1,34 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.controller.impl;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.authObj.AuthenticationService;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.response.JwtAuthResponse;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignIn;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignUp;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class User {
+
+    private final UserService userService;
+
+    private final AuthenticationService authenticationService;
+
+
+    //signup
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthResponse> signUp(@RequestBody SignUp signUp) {
+        return ResponseEntity.ok(authenticationService.signUp(signUp));
+    }
+
+    //signin
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthResponse> signIn(@RequestBody SignIn signIn) {
+        return ResponseEntity.ok(authenticationService.signIn(signIn));
+    }
+
+}
