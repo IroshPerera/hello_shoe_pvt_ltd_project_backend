48ffb29031683472f3c53cae549e4790699a5986
Irosh Perera <iroshlashan31@gmail.com>
2024-05-19 23:07:40 +0530

    few changes

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/EmployeeRepo.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/EmployeeRepo.java
@@ -2,8 +2,22 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.repository;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface EmployeeRepo extends JpaRepository<EmployeeEntity, String>{
+public interface EmployeeRepo extends JpaRepository<EmployeeEntity, String> {
+    @Query(value = "SELECT employee_code FROM employee ORDER BY employee_code DESC LIMIT 1", nativeQuery = true)
+    String getEmployeeCode();
+
+    @Modifying
+    @Query(value = "UPDATE employee SET active_state = ?1 WHERE employee_code = ?2", nativeQuery = true)
+    void changeActiveState(String deactivate, String employeeCode);
+
+    @Override
+    @Query(value = "SELECT * FROM employee WHERE active_state = 'ACTIVE'", nativeQuery = true)
+    List<EmployeeEntity> findAll();
 }
