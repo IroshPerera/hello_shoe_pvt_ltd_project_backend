dfcc31f8c3f444355667ee3597e3dcd6f731c0b8
Irosh Perera <iroshlashan31@gmail.com>
2024-05-19 23:07:21 +0530

    few changes

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SupplierService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SupplierService.java
@@ -3,4 +3,6 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SupplierDTO;
 
 public interface SupplierService extends SuperService<String, SupplierDTO> {
+    String generateSupplierID();
+
 }
