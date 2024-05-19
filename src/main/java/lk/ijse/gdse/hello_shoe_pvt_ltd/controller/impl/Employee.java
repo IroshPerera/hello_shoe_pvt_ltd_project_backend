2416278485a2d93927a50a07a3cfb7597627bc9f
Irosh Perera <iroshlashan31@gmail.com>
2024-05-19 23:08:15 +0530

    few changes

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/impl/Employee.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/impl/Employee.java
@@ -1,9 +1,13 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.controller.impl;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.controller.EmployeeController;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.BranchDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.EmployeeDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.EmployeeCountDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.EmployeeService;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Designation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/employee")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class Employee implements EmployeeController<String, EmployeeDTO>{
 
     private final EmployeeService employeeService;
@@ -20,7 +25,7 @@ public class Employee implements EmployeeController<String, EmployeeDTO>{
         return "Hello I'm Employee Controller. I'm OK! Have a nice day!";
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public boolean saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
         return employeeService.add(employeeDTO);
     }
@@ -43,5 +48,25 @@ public class Employee implements EmployeeController<String, EmployeeDTO>{
     @GetMapping("/all")
     public List<EmployeeDTO> getAllEmployees() {
         return employeeService.getAll();
+    }
+
+    @GetMapping("/branch")
+    public BranchDTO getBranch(@RequestParam String employee_code) {
+        return employeeService.getBranch(employee_code);
+    }
+
+    @GetMapping("/designations")
+    public List<Designation> getDesignations() {
+        return employeeService.getDesignations();
+    }
+
+    @GetMapping("/id")
+    public String getEmployeeId() {
+        return employeeService.getEmployeeCode();
+    }
+
+    @GetMapping("/count")
+    public EmployeeCountDTO getEmployeeCount() {
+        return employeeService.getEmployeeCount();
     }
 }
