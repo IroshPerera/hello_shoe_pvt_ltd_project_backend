4a706840359fa408f74c1dd5ed94a4c35fb548a0
Irosh Perera <iroshlashan31@gmail.com>
2024-05-19 23:07:41 +0530

    few changes

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/SizeRepo.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/SizeRepo.java
@@ -2,6 +2,12 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.repository;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.SizeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SizeRepo extends JpaRepository<SizeEntity,String> {
+    @Modifying
+    @Query(value = "UPDATE size SET active_state = ?1 WHERE size_code = ?2", nativeQuery = true)
+    void changeActiveState(String deactivate, String sizeCode);
+
 }
