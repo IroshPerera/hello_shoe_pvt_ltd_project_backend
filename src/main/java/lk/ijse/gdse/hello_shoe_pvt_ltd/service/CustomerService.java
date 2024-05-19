cfe12e0d08577ad9fc63a77cdbe936fdfab05be2
Irosh Perera <iroshlashan31@gmail.com>
2024-05-19 23:07:24 +0530

    few changes

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/CustomerService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/CustomerService.java
@@ -3,4 +3,5 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
 
 public interface CustomerService extends SuperService<String, CustomerDTO>{
+    String generateCustomerID();
 }
