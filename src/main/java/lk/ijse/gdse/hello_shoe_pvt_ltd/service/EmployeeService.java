f7cc8241029ca3130c1018142363b4b13e8f4f20
Irosh Perera <iroshlashan31@gmail.com>
2024-05-19 23:07:22 +0530

    few changes

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/EmployeeService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/EmployeeService.java
@@ -1,6 +1,19 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.BranchDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.EmployeeDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.EmployeeCountDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Designation;
+
+import java.util.List;
 
 public interface EmployeeService extends SuperService<String, EmployeeDTO> {
+    BranchDTO getBranch(String employeeCode);
+
+    String getEmployeeCode();
+
+    List<Designation> getDesignations();
+
+    EmployeeCountDTO getEmployeeCount();
+
 }
